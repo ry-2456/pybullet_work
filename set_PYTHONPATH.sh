5ec06c02051cbb8574f1f6c1f6981421bc91f69d
shinshu_taro <20w4067k@shinshu-u.ac.jp>
2021-01-12 16:41:36 +0900

    [change] make it possible to execute set_PYTHONPATH.sh from anywhere
    Note that it is expected that bullet3 and pybullet_work are in the same directory.

--- set_PYTHONPATH.sh
+++ set_PYTHONPATH.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-# for pybulelt settings
-export PYTHONPATH=~/programming/bullet3/build_cmake/examples/pybullet
+# set PYTHONPATH for pybullet
+# Note that bullet3 and pybullet_work must be in the same directory.
 
+# reference
+# bash/zshでsourceされたスクリプト内で~ https://qiita.com/yudoufu/items/48cb6fb71e5b498b2532 
+
+bullet_module_path="/bullet3/build_cmake/examples/pybullet"            # path to pybullet modules
+source_dir=$(cd $(dirname -- ${BASH_SOURCE:-0}); pwd)                  # set_PYTHONPATHのpath
+export PYTHONPATH=$(cd ${source_dir} && cd ../ && pwd)${bullet_module_path}
+
+# print PYTHONPATH
+yes = | head -n ${#PYTHONPATH} | tr -d '\n'
+echo
+echo "set PYTHONPATH as follows"
+echo ${PYTHONPATH}
+yes = | head -n ${#PYTHONPATH} | tr -d '\n'
+echo
