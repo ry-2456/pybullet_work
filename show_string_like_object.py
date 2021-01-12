2b3ea75b1118c21ca6d20e8dfa59986124b5b733
shinshu_taro <20w4067k@shinshu-u.ac.jp>
2021-01-12 17:02:43 +0900

    [change] use relative path instead of abs path to the model.

--- show_string_like_object.py
+++ show_string_like_object.py
@@ -21,19 +21,10 @@ if __name__ == "__main__":
 
     string_pos = [0.0, 0.5, 0.5]
     string_orn = p.getQuaternionFromEuler([0, 0, 0])
-    HOME_PATH = os.environ["HOME"]
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/string_object/model_hoge.sdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/string_object/model_huga.sdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/string_like_object_3.sdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/string_like_object.sdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/string_like_object_2.sdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/string_like_object_3.sdf"
-    # (stringId, ) = p.loadSDF(string_model_path)
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/hoge.urdf"
-    string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/string_like_object.urdf"
-    # string_model_path = HOME_PATH + "/programming/pybullet_work/data/urdf/humanoid.urdf"
-    stringId = p.loadURDF(string_model_path)# , useFixedBase=1)
-
+    string_model_path = "./data/urdf/string_like_object_3.sdf"
+    (stringId, ) = p.loadSDF(string_model_path)
+    # string_model_path = "./data/urdf/string_like_object.urdf"
+    # stringId = p.loadURDF(string_model_path)# , useFixedBase=1)
     p.resetBasePositionAndOrientation(stringId, string_pos, string_orn)
 
     p.setRealTimeSimulation(1)   # eable real-tiem simulation.
