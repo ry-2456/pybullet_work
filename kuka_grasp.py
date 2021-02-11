697a266ec17af135a114c75e878d6c88597ccfdf
rintaro-takashiro <ittanmomennimakaretyaimashita@gmail.com>
2021-02-11 23:40:36 +0900

    [add] クリックした物体を掴む処理を追加

--- kuka_grasp.py
+++ kuka_grasp.py
@@ -6,15 +6,20 @@
 # collisionFIlter.py
 # inverse_kinematics.py
 # pybullet_robots/baxter_ik_demo.py
+# createVisualShapeArray.py -- clickしたobjectのid取得
+# segmask_linkindex.py      -- segmaskからobject_id取得
 
 # 参照した情報
 # Applying force to grasp an object so that the object does not slip through the gripper #1936
 # https://github.com/bulletphysics/bullet3/issues/1936
+# https://stackoverflow.com/questions/59128880/getting-world-coordinates-from-opengl-depth-buffer
+
 # link_index == joint_index
 
 import cv2
 import pybullet as p
 import struct
+import time
 import pybullet_data
 import numpy as np
 import math
@@ -38,12 +43,24 @@ view_matrix = p.computeViewMatrix([-0.6, 0, 0.6], [-0.5, 0, 0], [1, 0, 0])
 projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
 
 # load block
+block_ids = []
 block_pos = [-0.5, 0, 0]
 block_orn = p.getQuaternionFromEuler([0,0,0])
 # block_pos = [1., 0, 0.5]
-blockId = p.loadURDF("block.urdf", block_pos, block_orn)
-p.resetBasePositionAndOrientation(blockId, block_pos, block_orn)
-block_pos[2] = 0.5
+# for i in range(4):
+#     # blockId = p.loadURDF("block.urdf", block_pos, block_orn)
+#     block_pos[0] -= i*0.1
+#     blockId = p.loadURDF("block.urdf", block_pos)
+#     block_ids.append(blockId)
+block_0_pos = [-0.5, 0.15, 0]
+block_1_pos = [-0.5, -0.15, 0]
+blockId0 = p.loadURDF("block.urdf", block_0_pos)
+blockId1 = p.loadURDF("block.urdf", block_1_pos, p.getQuaternionFromEuler([0,0,np.pi/2]))
+block_ids.append(blockId0)
+block_ids.append(blockId1)
+
+# p.resetBasePositionAndOrientation(blockId, block_pos, block_orn)
+# block_pos[2] = 0.5
 
 # load plane
 planeId = p.loadURDF("plane.urdf", useMaximalCoordinates=False)
@@ -56,7 +73,8 @@ leftfing1Id, leftfing2Id = 8, 10
 rightfing1Id, rightfing2Id = 11, 13
 # p.resetBasePositionAndOrientation(kukaId, [0,0,0], [0,0,0,1])
 
-p.changeDynamics(blockId, -1, lateralFriction=3, frictionAnchor=1)
+for b_id in block_ids:
+    p.changeDynamics(b_id, -1, lateralFriction=3, frictionAnchor=1)
 p.changeDynamics(kukaId, leftfing2Id, lateralFriction=3, frictionAnchor=1)
 p.changeDynamics(kukaId, rightfing2Id, lateralFriction=3, frictionAnchor=1)
 objectNum = p.getNumBodies()
@@ -71,7 +89,9 @@ for i in range(p.getNumJoints(kukaId)):
                         parentLinkIndex=i)
     # pprint.pprint(p.getJointInfo(kukaId, i)[3])
     print(p.getJointInfo(kukaId, i)[3])
-show_coords(blockId, frameLength=0.05)
+
+for b_id in block_ids:
+    show_coords(b_id, frameLength=0.05)
 
 #lower limits for null space
 ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
@@ -96,6 +116,40 @@ useSimulation = 1
 useRealTimeSimulation = 1
 ikSolver = 0
 p.setRealTimeSimulation(useRealTimeSimulation)
+
+def getRayFromTo(mouseX, mouseY):
+    width, height, viewMat, projMat, cameraUp, camForward, horizon, vertical, _, _, dist, camTarget = p.getDebugVisualizerCamera()
+    camPos = [
+        camTarget[0] - dist * camForward[0], 
+        camTarget[1] - dist * camForward[1],
+        camTarget[2] - dist * camForward[2]
+    ]
+    farPlane = 10000
+    rayForward = [(camTarget[0] - camPos[0]), (camTarget[1] - camPos[1]), (camTarget[2] - camPos[2])]
+    invLen = farPlane * 1. / (math.sqrt(rayForward[0] * rayForward[0] + 
+                                        rayForward[1] * rayForward[1] + 
+                                        rayForward[2] * rayForward[2]))
+    rayForward = [invLen * rayForward[0], invLen * rayForward[1], invLen * rayForward[2]]
+    rayFrom = camPos
+    oneOverWidth = float(1) / float(width)
+    oneOverHeight = float(1) / float(height)
+    dHor = [horizon[0] * oneOverWidth, horizon[1] * oneOverWidth, horizon[2] * oneOverWidth]
+    dVer = [vertical[0] * oneOverHeight, vertical[1] * oneOverHeight, vertical[2] * oneOverHeight]
+    rayToCenter = [rayFrom[0] + rayForward[0], rayFrom[1] + rayForward[1], rayFrom[2] + rayForward[2]]
+    rayTo = [
+        rayFrom[0] + rayForward[0] - 0.5 * horizon[0] + 0.5 * vertical[0] + float(mouseX) * dHor[0] -
+        float(mouseY) * dVer[0], rayFrom[1] + rayForward[1] - 0.5 * horizon[1] + 0.5 * vertical[1] +
+        float(mouseX) * dHor[1] - float(mouseY) * dVer[1], rayFrom[2] + rayForward[2] -
+        0.5 * horizon[2] + 0.5 * vertical[2] + float(mouseX) * dHor[2] - float(mouseY) * dVer[2]
+    ]
+    return rayFrom, rayTo
+
+def clicked_object_uid(mouse_x, mouse_y):
+    rayFrom, rayTo = getRayFromTo(mouse_x, mouse_y)
+    rayInfo = p.rayTest(rayFrom, rayTo)
+    hit = rayInfo[0]
+    object_uid = hit[0]
+    return object_uid
 
 def copy_img(dst, src):
     """
@@ -299,8 +353,10 @@ def move_eff(target_pos, target_orn):
                                 velocityGain=1)
 
 if __name__ == "__main__":
-    is_grasping = True
-    grasp()
+    # is_grasping = True
+    # grasp()
+    is_grasping = False
+    release()
 
     eef_pos, eef_orn = p.getLinkState(kukaId, kukaEndEffectorIndex)[4:6]
     eef_orn = p.getEulerFromQuaternion(eef_orn)
@@ -315,7 +371,7 @@ if __name__ == "__main__":
     # for gripper 
     eef_init = 0
     eef_rot_id = p.addUserDebugParameter("eef", -3.14, 3.14, eef_init)
-
+    done_init = False
     while True:
         eef_x = p.readUserDebugParameter(eef_x_id)
         eef_y = p.readUserDebugParameter(eef_y_id)
@@ -327,7 +383,11 @@ if __name__ == "__main__":
         # targetAngleTip = p.readUserDebugParameter(targetAngleTipId)
         pos = [eef_x, eef_y, eef_z]
         orn = [eef_roll, eef_pitch, eef_yaw]
-        move_eff(pos, orn)
+        # move_eff(pos, orn)
+        if not done_init:
+            pos[2] = 0.5
+            move_eff(pos, [0,-np.pi,0])
+            done_init = True
         
         eef_rot = p.readUserDebugParameter(eef_rot_id)
         numJoints = p.getNumJoints(kukaId)
@@ -341,34 +401,74 @@ if __name__ == "__main__":
                 grasp()
                 is_grasping = True
 
-        mouse_events = p.getMouseEvents()
-        if mouse_events:
-            print(mouse_events)
-
         images = p.getCameraImage(width,
                                   height,
                                   view_matrix,
                                   projection_matrix,
                                   # shadow=True,
                                   renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+        # クリックされたobject_uidを取得
+        object_uid = -1
+        mouse_events = p.getMouseEvents()
+        for e in mouse_events:
+            if ((e[0] == 2) and (e[3] == 0) and (e[4] & p.KEY_WAS_TRIGGERED)): # 左クリック判定
+                mouse_x = e[1]
+                mouse_y = e[2]
+                object_uid = clicked_object_uid(mouse_x, mouse_y) # -1 for no object was clicked 
         
-        b_pos = get_block_pos(blockId, images)
-        print(b_pos)
-        print(block_pos)
-    
+        # print(object_uid)
+        if object_uid < 0: continue # no object was clicked 
+        if object_uid not in block_ids: continue # a block was not clicked 
+        
+        b_pos = get_block_pos(object_uid, images)
         if b_pos is None: continue
-        # blockの長手方向を描画
+
+        if object_uid == blockId0: print(block_0_pos)
+        if object_uid == blockId1: print(block_1_pos)
+        print(b_pos)
+
+        # ブロックの長手方向の計算
         depth_buffer = images[3].reshape(height, width)
         seg = images[4].reshape(height, width)
-        block_mask = object_mask_from_seg(blockId, seg)
+        block_mask = object_mask_from_seg(object_uid, seg)
         posmap = get_posmap(near, far, 
                             view_matrix, projection_matrix, 
                             height, width, depth_buffer)
         block_posmap = posmap[block_mask!=0]
-        plt.scatter(block_posmap[:,0].reshape(-1), block_posmap[:,1],s=10)
-        plt.xlim(-2,2)
-        plt.ylim(-2,2)
-        plt.grid()
-        plt.show()
         v = longitudinal_direction(block_posmap)
+
+        # world -> eefの同時変換行列を計算
+        eef_base_pos, eef_base_orn = p.getLinkState(kukaId, kukaEndEffectorIndex)[4:6]
+        world_to_eef_base = np.eye(4)
+        world_to_eef_base[0:3,0:3] = np.array(p.getMatrixFromQuaternion(eef_base_orn)).reshape(3,3)
+        world_to_eef_base[:3,3] = eef_base_pos
+
+        # yawの回転角の計算, eefのy軸が方向にブロックの長手方向が向いていればいい
+        v_in_eef = np.linalg.inv(world_to_eef_base).dot(np.hstack([v,1]))[:3] - np.linalg.inv(world_to_eef_base).dot(np.array([0,0,0,1]))[:3]
+        v_in_eef = v_in_eef / np.linalg.norm(v_in_eef)
+        e_y = np.array([0,1.,0])
+        yaw_rot_diff = np.arccos(np.dot(v_in_eef, e_y))
+        # print(yaw_rot_diff)
+        # print(np.rad2deg(yaw_rot_diff))
+        yaw_now = p.getEulerFromQuaternion(eef_base_orn)[2]
+        eef_orn = [0, -np.pi, yaw_now] # eefが下向き
+        if v_in_eef[0] < 0: eef_orn[2] -= yaw_rot_diff
+        else:               eef_orn[2] += yaw_rot_diff
+        # TODO : eef_orn[2]を範囲に収める処理を追加
+
+        release()
+        time.sleep(0.1)
+        move_eff([-0.5,0,0.5], eef_orn)
+        time.sleep(2)
+        b_pos[2] = -0.005+0.1
+        move_eff(b_pos, eef_orn)
+        time.sleep(2)
+        b_pos[2] = -0.005
+        move_eff(b_pos, eef_orn)
+        time.sleep(2)
+        grasp()
+        time.sleep(0.1)
+    
+        # # blockの長手方向を描画
         p.addUserDebugLine(b_pos+v/10, b_pos-v/10, [1, 0, 0])
